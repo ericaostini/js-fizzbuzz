a316e8dc49dbf457076739836cebb4ac050907c6
ericaostini <ericaostini14@gmail.com>
2023-10-11 15:50:57 +0200

    css box backgrounds and set if/else conditions

--- js/script.js
+++ js/script.js
@@ -2,5 +2,13 @@ const container = document.getElementById("container");
 
 for(let i = 1; i <= 100; i++){
     console.log(i);
-    
+    if (i % 3 === 0){
+        boxColor = "fizz";
+    } else if (i % 5 === 0){
+        boxColor = "buzz";
+    } else if (i % 3 === 0 && i % 5 === 0){
+        boxColor = "fizzBuzz";
+    } else{
+        boxColor = "null";
+    }
 }
