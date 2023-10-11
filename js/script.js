875dbfd162665ee7894237f054a8a71ce2d94861
ericaostini <ericaostini14@gmail.com>
2023-10-11 17:21:02 +0200

    fixed display box with d-flex

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
+const wrapper = document.getElementById("wrapper");
 const container = document.getElementById("container");
-
 for(let i = 1; i <= 100; i++){
     console.log(i);
     if (i % 3 === 0){
-        boxColor = "fizz";
+        boxCol = "fizz";
     } else if (i % 5 === 0){
-        boxColor = "buzz";
-    } else if (i % 3 === 0 && i % 5 === 0){
-        boxColor = "fizzBuzz";
-    } else{
-        boxColor = "null";
+        boxCol = "buzz";
+    } else if(i % 3 === 0 && i % 5 === 0){
+        boxCol = "fizzBuzz";
+    } else {
+        boxCol = "null";
     }
     const box = document.createElement("div");
+    box.classList.add("square");
+    container.append(box);
     console.log(box);
-    box.innerHTML = `square ${boxColor}`;
 }
 
 
+    
 
+// if (i % 3 === 0){
+//     box.classList.add("fizz");
+// } else if (i % 5 === 0){
+//     box.classList.add("buzz");
+// } else if (i % 3 === 0 && i % 5 === 0){
+//     box.classList.add("fizzBuzz");
+// } else{
+//     box.classList.add("null");
+// }
 
