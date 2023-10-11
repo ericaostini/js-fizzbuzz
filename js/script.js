8c787d3cdd1c00ae3c237e39736573bf7cc44a84
ericaostini <ericaostini14@gmail.com>
2023-10-11 17:43:42 +0200

    display text in box

--- js/script.js
+++ js/script.js
@@ -4,15 +4,22 @@ for(let i = 1; i <= 100; i++){
     console.log(i);
     if (i % 3 === 0 && i % 5 !== 0){
         boxCol = "fizz";
+        text = "fizz";
     } else if (i % 5 === 0 && i % 3 !== 0){
         boxCol = "buzz";
+        text = "buzz";
     } else if(i % 3 === 0 && i % 5 === 0){
         boxCol = "fizzBuzz";
+        text = "fizzBuzz";
     } else {
         boxCol = "null";
+        text = i;
     }
     const box = document.createElement("div");
-    box.className = `square ${boxCol}`;
+    box.className = `square ${boxCol} d-flex align-content-center justify-content-center`;
+    box.innerHTML = `
+    <p> ${text} </p>
+    `;
     container.append(box);
     console.log(box);
 }
