ca6a8a3c31a80412c1371807be93cf4cf2059e74
ericaostini <ericaostini14@gmail.com>
2023-10-12 09:57:22 +0200

    correct js code

--- js/script-nodiv.js
+++ js/script-nodiv.js
@@ -3,8 +3,11 @@ const container = document.createElement("div");
 container.className = `container d-flex flex-wrap justify-content-center p-6`;
 wrapper.append(container);
 
+// per 100 blocchi display fizz se divisibile per 3, buzz se divisibile per 5, fizzBuzz se divisibile sia per 3 che per 5 e il numero se non è divisibile per i due numeri 
+
 for(let i = 1; i <= 100; i++){
     console.log(i);
+    let boxCol, text;
     if (i % 3 === 0 && i % 5 !== 0){
         boxCol = "fizz";
         text = "fizz";
@@ -21,7 +24,7 @@ for(let i = 1; i <= 100; i++){
     const box = document.createElement("div");
     box.className = `square ${boxCol} d-flex align-items-center justify-content-center m-4`;
     box.innerHTML = `
-    <p> ${text} </p>
+    <p class = "fw-bolder"> ${text} </p>
     `;
     container.append(box);
     console.log(box);
